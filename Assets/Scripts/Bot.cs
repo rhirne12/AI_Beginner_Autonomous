df1ffbd279c1a95a97f4a06d864089829fedff09
rhirne12 <126825699+rhirne12@users.noreply.github.com>
2023-04-14 16:49:02 -0400

    Hiding
    - beginning Hiding part 2

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -9,6 +9,8 @@ public class Bot : MonoBehaviour
     public GameObject target;
     Drive ds;
 
+    Vector3 wanderTarget = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,26 +51,56 @@ public class Bot : MonoBehaviour
     {
         Vector3 targetDir = target.transform.position - this.transform.position;
 
-        // float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
-        // float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
-
-
-        //if ((toTarget > 90 && relativeHeading < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
-        //{
-        //    Flee(target.transform.position);
-        //    return;
-        //}
-
         float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
         Flee(target.transform.position + target.transform.forward * lookAhead);
+    }
 
+    void Wander()
+    {
+        float wanderRadius = 10;     // Size of the imaginary circle in front of navmeshagent
+        float wanderDistance = 10;   // Distance from Agent to the middle of the imaginary circle in front of him
+        float wanderJitter = 1;      // How the target location moves around from update to update
 
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);  // Target is in the direction of the imaginary circle, but not ON the circle
+
+        wanderTarget.Normalize();         // Brings it down to a length of 1
+        wanderTarget *= wanderRadius;     // pushes it out to the correct length
+
+        // Circle is now AROUND the agent, with a target on the circle.  Agent is at local position of 0,0,0
+        // Need to create a vector3 postion in front of agent
+
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);                     //  Moves local target out
+        Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);       //  changes local target to a world target that can be seeked  
+
+        Seek(targetWorld);
+
+    }
+
+    void Hide()
+    {
+        // Find Closest hiding spot
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+        for (int i=0; i< World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;  // Vector from the Cop to the Tree hiding spot
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 10;  // Vector to hiding spot behind tree hiding spot
+
+            if (Vector3.Distance(this.transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Seek(chosenSpot);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Flee(target.transform.position);
-        Evade();
+        Hide();
     }
 }
